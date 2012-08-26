531455411c95d00bd4745c77d31d9bbcc40c5ed5
Tony Wang <wwwjfy@gmail.com>
2012-08-27 00:09:19 +0800

    adding posts function

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
+import time
+
 from flask import Flask
 from flask import render_template
+from flask import request
 from flaskext.markdown import Markdown
 
 from config import config
 import utils
 
+content_template = """\
+# %(title)s
+
+%(time)s
+
+%(content)s
+"""
+
 
 app = Flask(__name__)
-md = Markdown(app, extensions=['attr_list', 'fenced_code'])
+Markdown(app, extensions=['attr_list', 'fenced_code'])
 
 
 @app.route('/')
 def index():
-    return render_template('admin/index.html', config=config)
+    infos = utils.get_post_infos()
+
+    for info in infos:
+        with open('posts/%s' % info['filename'], 'r') as f:
+            content = f.read().decode(config['encoding'])
+            title = utils.get_title(content)
+            info['title'] = title
+            info['date'] = time.strftime(config['datetime_format'], time.localtime(info['time']))
+    return render_template('admin/index.html', config=config, infos=infos)
 
 
 @app.route('/generate')
@@ -35,7 +54,8 @@ def generate():
     for info in infos:
         with open('posts/%s' % info['filename'], 'r') as f:
             content = f.read().decode(config['encoding'])
-            title, content = utils.postprocess_post_content(info['slug'],
+            title = utils.get_title(content)
+            content = utils.postprocess_post_content(info['slug'],
                                                             content, False)
             html_content = render_template('frontend/post.html',
                                            config=config,
@@ -45,6 +65,54 @@ def generate():
                                     html_content.encode(config['encoding']))
 
     return 'Done!'
+
+
+@app.route('/edit/<time_slug>', methods=['GET', 'POST'])
+def edit(time_slug):
+    if request.method == 'GET':
+        filename = '%s.md' % time_slug
+        file_path = 'posts/%s' % filename
+        import os
+        if not os.path.exists(file_path):
+            # TODO: 404 page
+            return '', 404
+        info = utils.parse_filename(filename)
+        with open(file_path) as f:
+            content = f.read().decode(config['encoding'])
+            info['title'] = utils.get_title(content)
+            info['content'] = '\n'.join(content.splitlines()[5:])
+            info['date'] = time.strftime(config['datetime_format'], time.localtime(info['time']))
+
+        return render_template('admin/edit.html',
+                               config=config,
+                               info=info)
+    elif request.method == 'POST':
+        return ''
+
+
+@app.route('/new', methods=['GET', 'POST'])
+def new():
+    if request.method == 'GET':
+        info = {'title': 'New post', 'new': '1'}
+        return render_template('admin/edit.html',
+                               config=config,
+                               info=info)
+    elif request.method == 'POST':
+        title = request.form['title'].strip()
+        content = request.form['content'].strip()
+        slug = request.form['slug'].strip()
+        post_time = int(time.time())
+        time_str = time.strftime(config['datetime_format'],
+                                 time.localtime(post_time))
+
+        file_content = content_template % {'title': title,
+                                           'time': time_str,
+                                           'content': content}
+        file_path = 'posts/%s-%s.md' % (post_time, slug)
+        with open(file_path, 'w') as f:
+            f.write(file_content)
+
+        return ''
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -7,20 +7,44 @@ from config import config
 legal_post_file_name = re.compile(r'(^[0-9]+)-(.*)\.md$')
 
 
+def parse_filename(name):
+    match = legal_post_file_name.match(name)
+    if match is not None:
+        return {'filename': match.group(0),
+                'time': int(match.group(1)),
+                'slug': match.group(2)}
+
+
 def sort_post_infos(infos):
-    infos.sort(key=lambda info: info['time'])
+    infos.sort(key=lambda info: info['time'], reverse=True)
+
+
+def get_post_infos(with_title=False):
+    preprocessed_names = os.walk('posts').next()[2]
+    infos = []
+    for name in preprocessed_names:
+        result = parse_filename(name)
+        if result:
+            infos.append(result)
+    sort_post_infos(infos)
+    return infos
+
+
+def get_title(content):
+    lines = content.splitlines()
+    title_line = lines[0]
+    if not title_line.startswith('# '):
+        assert('Unsupported format: first line is not started with "# "')
+    return title_line[2:]
 
 
 def postprocess_post_content(slug, content, title_with_link):
+    title = get_title(content)
     lines = content.splitlines()
-    title_line, time_line = lines[0], lines[2]
-    if not title_line.startswith('# '):
-        assert('Unsupported format: first line is not started with "# "')
-    title = title_line[2:]
     if title_with_link:
         lines[0] = '# [%s](/posts/%s)' % (title, slug)
     lines.insert(3, '{: .time }')
-    return title, '\n'.join(lines)
+    return '\n'.join(lines)
 
 
 def clear_dir(directory):
@@ -30,19 +54,6 @@ def clear_dir(directory):
                 os.remove(os.path.join(dirpath, filename))
 
 
-def get_post_infos():
-    preprocessed_names = os.walk('posts').next()[2]
-    infos = []
-    for name in preprocessed_names:
-        match = legal_post_file_name.match(name)
-        if match is not None:
-            infos.append({'filename': match.group(0),
-                          'time': int(match.group(1)),
-                          'slug': match.group(2)})
-    sort_post_infos(infos)
-    return infos
-
-
 def get_posts():
     infos = get_post_infos()
 
@@ -50,6 +61,6 @@ def get_posts():
     for info in infos:
         with open('posts/%s' % info['filename'], 'r') as f:
             content = f.read().decode(config['encoding'])
-            _, content = postprocess_post_content(info['slug'], content, True)
+            content = postprocess_post_content(info['slug'], content, True)
             posts.append(content)
     return posts
